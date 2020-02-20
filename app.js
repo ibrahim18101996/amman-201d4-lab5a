ca7297696826e25a12ef766f9a3820599deffa93
ibrahim18101996 <brhomd.96@gmail.com>
2020-02-20 12:14:17 +0200

    multiplt added

--- app.js
+++ app.js
@@ -31,11 +31,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+var theMultiply = a*b;
+var message1 = `The product of ${a} and ${b} is ${theMultiply}.`;
+return [theMultiply, message1];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
