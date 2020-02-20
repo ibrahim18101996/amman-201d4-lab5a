545bcade18e09b648549095f177bbf00a118ff86
ibrahim18101996 <brhomd.96@gmail.com>
2020-02-20 14:45:00 +0200

    multiply array is done

--- app.js
+++ app.js
@@ -111,11 +111,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+var dota = sumAndMultiply(multArr[0],multArr[1],multArr[2]);
+var dotb = dota[1];
+var mg1 = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${dotb}.`;
+return [dotb,mg1];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
